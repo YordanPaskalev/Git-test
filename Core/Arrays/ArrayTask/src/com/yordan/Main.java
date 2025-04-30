01eff6d2dcff3db8322ac6b2eb35b5957d2880ae
Yordan Paskalev <ypaskalev@gmail.com>
2025-04-30 20:39:10 +0300

    test2

--- Core/Arrays/ArrayTask/src/com/yordan/Main.java
+++ Core/Arrays/ArrayTask/src/com/yordan/Main.java
@@ -3,5 +3,9 @@ package com.yordan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Now I can continue make changes to my project :)");
     }
+
+
+
 }
